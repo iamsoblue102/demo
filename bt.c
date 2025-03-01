3c6e397e1be4d7b2083dea4af109bfced37dbdc5
minhphu0168 <60965386+minhphu0168@users.noreply.github.com>
2025-03-01 13:45:45 +0700

    Add files via upload

---
+++ bt.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    int list1[] = {1, 2, 4};
+    int list2[] = {1, 3, 4};
+    int list_output[6];
+
+    int i = 0, j = 0, k = 0;
+
+    while ((i < 3) && (j < 3)){
+        if (list1[i] < list2[j]){
+            list_output[k] = list1[i];
+            k++;
+            i++;
+        }
+        else if (list1[i] > list2[j]){
+            list_output[k] = list2[j];
+            k++;
+            j++;
+        }
+        else{
+            list_output[k] = list2[j];
+            list_output[k+1] = list1[i];
+            k += 2;
+            i++;
+            j++;
+        }
+    }
+
+    for (int f = 0; f < 6; f++)
+    {
+        printf("%d\t", list_output[f]);
+    }
+}
